d5c90239f0b5dd9b0a4e83cbdc2e92cd00952c40
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-08-22 19:29:27 +0200

    renamed output image

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -e -u
 
-iso_name=archlinux
-iso_label="ARCH_$(date +%Y%m)"
+iso_name=bbqlinux
+iso_label="BBQLINUX_$(date +%Y%m)"
 iso_version=$(date +%Y.%m.%d)
 install_dir=arch
 arch=$(uname -m)
