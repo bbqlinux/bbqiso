a346eb26855bb2fcbec197cef3b70eb734eb74dd
codeworkx <codeworkx@cyanogenmod.org>
2016-09-20 19:33:00 +0200

    calculate efiboot size

--- build.sh
+++ build.sh
@@ -217,42 +217,71 @@ make_efi() {
     curl -o ${work_dir}/iso/EFI/shellx64_v2.efi https://raw.githubusercontent.com/tianocore/edk2/master/ShellBinPkg/UefiShell/X64/Shell.efi
     # EFI Shell 1.0 for non UEFI 2.3+
     curl -o ${work_dir}/iso/EFI/shellx64_v1.efi https://raw.githubusercontent.com/tianocore/edk2/master/EdkShellBinPkg/FullShell/X64/Shell_Full.efi
-
-
 }
 
 # Prepare efiboot.img::/EFI for "El Torito" EFI boot mode
 make_efiboot() {
+
+    # Path definitions
+    P_VMLINUZ="${work_dir}/iso/${install_dir}/boot/x86_64/vmlinuz"
+    P_ARCHISO="${work_dir}/iso/${install_dir}/boot/x86_64/archiso.img"
+    P_INTEL_UCODE="${work_dir}/iso/${install_dir}/boot/intel_ucode.img"
+    P_PRELOADER="${work_dir}/x86_64/airootfs/usr/share/efitools/efi/PreLoader.efi"
+    P_HASHTOOL="${work_dir}/x86_64/airootfs/usr/share/efitools/efi/HashTool.efi"
+    P_SYSTEMD_BOOT="${work_dir}/x86_64/airootfs/usr/lib/systemd/boot/efi/systemd-bootx64.efi"
+    P_LOADER_CONF="${script_path}/efiboot/loader/loader.conf"
+    P_UEFI_SHELL_V1="${work_dir}/iso/EFI/shellx64_v1.efi"
+    P_UEFI_SHELL_V1_CONF="${script_path}/efiboot/loader/entries/uefi-shell-v2-x86_64.conf"
+    P_UEFI_SHELL_V2="${work_dir}/iso/EFI/shellx64_v2.efi"
+    P_UEFI_SHELL_V2_CONF="${script_path}/efiboot/loader/entries/uefi-shell-v1-x86_64.conf"
+
+    # Calculate efiboot size
+    efiboot_size=$(( $(wc -c < "$P_VMLINUZ") \
+                     + $(wc -c < "$P_ARCHISO") \
+                     + $(wc -c < "$P_INTEL_UCODE") \
+                     + $(wc -c < "${P_PRELOADER}") \
+                     + $(wc -c < "${P_HASHTOOL}") \
+                     + $(wc -c < "${P_SYSTEMD_BOOT}") \
+                     + $(wc -c < "${P_LOADER_CONF}") \
+                     + $(wc -c < "${P_UEFI_SHELL_V1}") \
+                     + $(wc -c < "${P_UEFI_SHELL_V1_CONF}") \
+                     + $(wc -c < "${P_UEFI_SHELL_V2}") \
+                     + $(wc -c < "${P_UEFI_SHELL_V2_CONF}") ))
+
+    efiboot_size=$(( ${efiboot_size} / 1024 / 1024 )) # Convert to M
+    efiboot_size=$(( ${efiboot_size} +  (${efiboot_size} * 0,2) ))"M"
+    echo "EFIBOOT SIZE: ${efiboot_size}"
+
     mkdir -p ${work_dir}/iso/EFI/archiso
-    truncate -s 40M ${work_dir}/iso/EFI/archiso/efiboot.img
+    truncate -s ${efiboot_size} ${work_dir}/iso/EFI/archiso/efiboot.img
     mkfs.fat -n ARCHISO_EFI ${work_dir}/iso/EFI/archiso/efiboot.img
 
     mkdir -p ${work_dir}/efiboot
     mount ${work_dir}/iso/EFI/archiso/efiboot.img ${work_dir}/efiboot
 
     mkdir -p ${work_dir}/efiboot/EFI/archiso
-    cp ${work_dir}/iso/${install_dir}/boot/x86_64/vmlinuz ${work_dir}/efiboot/EFI/archiso/vmlinuz.efi
-    cp ${work_dir}/iso/${install_dir}/boot/x86_64/archiso.img ${work_dir}/efiboot/EFI/archiso/archiso.img
+    cp ${P_VMLINUZ} ${work_dir}/efiboot/EFI/archiso/vmlinuz.efi
+    cp ${P_ARCHISO} ${work_dir}/efiboot/EFI/archiso/archiso.img
     
-    cp ${work_dir}/iso/${install_dir}/boot/intel_ucode.img ${work_dir}/efiboot/EFI/archiso/intel_ucode.img
+    cp ${P_INTEL_UCODE} ${work_dir}/efiboot/EFI/archiso/intel_ucode.img
 
     mkdir -p ${work_dir}/efiboot/EFI/boot
-    cp ${work_dir}/x86_64/airootfs/usr/share/efitools/efi/PreLoader.efi ${work_dir}/efiboot/EFI/boot/bootx64.efi
-    cp ${work_dir}/x86_64/airootfs/usr/share/efitools/efi/HashTool.efi ${work_dir}/efiboot/EFI/boot/
+    cp ${P_PRELOADER} ${work_dir}/efiboot/EFI/boot/bootx64.efi
+    cp ${P_HASHTOOL} ${work_dir}/efiboot/EFI/boot/
 
-    cp ${work_dir}/x86_64/airootfs/usr/lib/systemd/boot/efi/systemd-bootx64.efi ${work_dir}/efiboot/EFI/boot/loader.efi
+    cp ${P_SYSTEMD_BOOT} ${work_dir}/efiboot/EFI/boot/loader.efi
 
     mkdir -p ${work_dir}/efiboot/loader/entries
-    cp ${script_path}/efiboot/loader/loader.conf ${work_dir}/efiboot/loader/
-    cp ${script_path}/efiboot/loader/entries/uefi-shell-v2-x86_64.conf ${work_dir}/efiboot/loader/entries/
-    cp ${script_path}/efiboot/loader/entries/uefi-shell-v1-x86_64.conf ${work_dir}/efiboot/loader/entries/
+    cp ${P_LOADER_CONF} ${work_dir}/efiboot/loader/
+    cp ${P_UEFI_SHELL_V2_CONF} ${work_dir}/efiboot/loader/entries/
+    cp ${P_UEFI_SHELL_V2_CONF} ${work_dir}/efiboot/loader/entries/
 
     sed "s|%ARCHISO_LABEL%|${iso_label}|g;
          s|%INSTALL_DIR%|${install_dir}|g" \
         ${script_path}/efiboot/loader/entries/archiso-x86_64-cd.conf > ${work_dir}/efiboot/loader/entries/archiso-x86_64.conf
 
-    cp ${work_dir}/iso/EFI/shellx64_v2.efi ${work_dir}/efiboot/EFI/
-    cp ${work_dir}/iso/EFI/shellx64_v1.efi ${work_dir}/efiboot/EFI/
+    cp ${P_UEFI_SHELL_V2} ${work_dir}/efiboot/EFI/
+    cp ${P_UEFI_SHELL_V1} ${work_dir}/efiboot/EFI/
 
     umount -d ${work_dir}/efiboot
 }
